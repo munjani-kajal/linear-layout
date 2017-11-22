8a68761741f5e28183f592cd84eecb1d38533bad
munjani-kajal <33296314+munjani-kajal@users.noreply.github.com>
2017-11-22 15:46:17 -0500

    Add files via upload

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,141 +1,85 @@
-﻿using System;
-using Android.App;
-using Android.OS;
-using Android.Gms.Location;
-using Android.Gms.Common;
-using Android.Gms.Common.Apis;
-using Android.Util;
+﻿using Android.App;
 using Android.Widget;
-using Android.Locations;
+using Android.OS;
+using Android.Provider;
+using Java.IO;
+using Android.Graphics;
+using Android.Content.PM;
+using Uri = Android.Net.Uri;
+using System.Collections.Generic;
+using Android.Content;
+using System;
+using Android;
 
-namespace App5
+namespace App2
 
 {
-    [Activity(Label = "App5", MainLauncher = true)]
-    public class MainActivity : Activity, GoogleApiClient.IConnectionCallbacks,
-        GoogleApiClient.IOnConnectionFailedListener, Android.Gms.Location.ILocationListener
+    [Activity(Label = "App2", MainLauncher = true)]
+    public class MainActivity : Activity
     {
-        GoogleApiClient apiClient;
-        LocationRequest locRequest;
-        Button button2;
-        TextView latitude2;
-        TextView longitude2;
-        TextView provider2;
+        Button BtnTakeImg;
+        ImageView ImgView;
+        public static File _file;
+        public static File _dir;
 
-        ////Lifecycle methods
-
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-            // UI to print location updates
-            button2 = FindViewById<Button>(Resource.Id.myButton2);
-            latitude2 = FindViewById<TextView>(Resource.Id.latitude2);
-            longitude2 = FindViewById<TextView>(Resource.Id.longitude2);
-            provider2 = FindViewById<TextView>(Resource.Id.provider2);
-
-            // pass in the Context, ConnectionListener and ConnectionFailedListener
-            apiClient = new GoogleApiClient.Builder(this, this, this).AddApi(LocationServices.API).Build();
-
-            // generate a location request that we will pass into a call for location updates
-            locRequest = new LocationRequest();
-
-        }
-
-        protected override void OnResume()
-        {
-            base.OnResume();
-            Log.Debug("OnResume", "OnResume called, connecting to client...");
-
-            apiClient.Connect();
-
-            // Clicking the second button will send a request for continuous updates
-            button2.Click += async delegate {
-                if (apiClient.IsConnected)
-                {
-                    button2.Text = "Requesting Location Updates";
-
-                    // Setting location priority to PRIORITY_HIGH_ACCURACY (100)
-                    locRequest.SetPriority(100);
-
-                    // Setting interval between updates, in milliseconds
-                    // NOTE: the default FastestInterval is 1 minute. If you want to receive location updates more than 
-                    // once a minute, you _must_ also change the FastestInterval to be less than or equal to your Interval
-                    locRequest.SetFastestInterval(500);
-                    locRequest.SetInterval(1000);
-
-                    // pass in a location request and LocationListener
-                    await LocationServices.FusedLocationApi.RequestLocationUpdates(apiClient, locRequest, this);
-                    // In OnLocationChanged (below), we will make calls to update the UI
-                    // with the new location data
-                }
-                else
-                {
-                    Log.Info("LocationClient", "Please wait for Client to connect");
-                }
-            };
-        }
-
-        protected override async void OnPause()
-        {
-            base.OnPause();
-
-            if (apiClient.IsConnected)
+            if (IsThereAnAppToTakePictures())
             {
-                // stop location updates, passing in the LocationListener
-                await LocationServices.FusedLocationApi.RemoveLocationUpdates(apiClient, this);
+                CreateDirectoryForPictures();
+                BtnTakeImg = FindViewById<Button>(Resource.Id.btntakepicture);
+                ImgView = FindViewById<ImageView>(Resource.Id.ImgTakeimg);
+                BtnTakeImg.Click += TakeAPicture;
+            }
 
-                apiClient.Disconnect();
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            _dir = new File(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures), "C#Corner");
+            if (!_dir.Exists())
+            {
+                _dir.Mkdirs();
             }
         }
-
-
-        ////Interface methods
-
-        public void OnConnected(Bundle bundle)
+        private bool IsThereAnAppToTakePictures()
         {
-            // This method is called when we connect to the LocationClient. We can start location updated directly form
-            // here if desired, or we can do it in a lifecycle method, as shown above 
-
-            // You must implement this to implement the IGooglePlayServicesClientConnectionCallbacks Interface
-
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities = PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
         }
-
-        public void OnDisconnected()
+        private void TakeAPicture(object sender, EventArgs eventArgs)
         {
-            // This method is called when we disconnect from the LocationClient.
-
-            // You must implement this to implement the IGooglePlayServicesClientConnectionCallbacks Interface
-
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            _file = new File(_dir, string.Format("Image_{0}.jpg", Guid.NewGuid()));
+            intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(_file));
+            StartActivityForResult(intent, 102);
         }
-
-        public void OnConnectionFailed(ConnectionResult bundle)
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
-            // This method is used to handle connection issues with the Google Play Services Client (LocationClient). 
-            // You can check if the connection has a resolution (bundle.HasResolution) and attempt to resolve it
-
-            // You must implement this to implement the IGooglePlayServicesClientOnConnectionFailedListener Interface
-
-        }
-
-        public void OnLocationChanged(Location location)
-        {
-            // This method returns changes in the user's location if they've been requested
-
-            // You must implement this to implement the Android.Gms.Locations.ILocationListener Interface
-            Log.Debug("LocationClient", "Location updated");
-
-            latitude2.Text = "Latitude: " + location.Latitude.ToString();
-            longitude2.Text = "Longitude: " + location.Longitude.ToString();
-            provider2.Text = "Provider: " + location.Provider.ToString();
-        }
-
-        public void OnConnectionSuspended(int i)
-        {
-
+            if (requestCode == 102 && resultCode == Result.Ok)
+            {
+                // make it available in the gallery  
+                Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+                Uri contentUri = Uri.FromFile(_file);
+                mediaScanIntent.SetData(contentUri);
+                SendBroadcast(mediaScanIntent);
+                //Converstion Image Size  
+                int height = ImgView.Height;
+                int width = Resources.DisplayMetrics.WidthPixels;
+                using (Bitmap bitmap = _file.Path.LoadAndResizeBitmap(width, height))
+                {
+                    //View ImageView  
+                    ImgView.RecycleBitmap();
+                    ImgView.SetImageBitmap(bitmap);
+                    //Upload Image in Database  
+                }
+            }
         }
     }
 }
+
